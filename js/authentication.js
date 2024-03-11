0a8603f6be140e8f4971d3fe4f54cb4f5226530f
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2024-03-12 00:41:55 +0200

    Initialize Firebase and configuration

---
+++ js/authentication.js
@@ -0,0 +1,15 @@
+//  firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyC5N2hdVrUxwSFaO_CZ_R_tOk9U_il01zo",
+  authDomain: "movie-app-fa88c.firebaseapp.com",
+  projectId: "movie-app-fa88c",
+  storageBucket: "movie-app-fa88c.appspot.com",
+  messagingSenderId: "740073435661",
+  appId: "1:740073435661:web:12a0c93fca229c6431db44",
+  measurementId: "G-NGYMYW7RT2",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// Initialize variables
+const auth = firebase.auth();
+const database = firebase.database();
