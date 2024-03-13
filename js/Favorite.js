3ba009a8d0279120c936dd2f8ce9d7c7ff0a1ba1
safaaabdelnaser <safaaabdelnaser76@gmail.com>
2024-03-13 13:06:41 +0200

    Function to remove a favorite movie from favorite page

--- js/Favorite.js
+++ js/Favorite.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
   const favoriteMoviesContainer = document.getElementById("movies-container");
-
   // Retrieve favorite movies from local storage
   const favoriteMovies =
     JSON.parse(localStorage.getItem("favoriteMovies")) || [];
   // Display favorite movies
   displayFavoriteMovies(favoriteMovies);
-
   function displayFavoriteMovies(movies) {
     favoriteMoviesContainer.innerHTML = "";
     movies.forEach((movie) => {
@@ -14,10 +12,17 @@ document.addEventListener("DOMContentLoaded", function () {
       card.classList.add("card");
       const title = document.createElement("span");
       title.textContent = movie.title;
+
+      // Create favorite button
       const favoriteButton = document.createElement("button");
       const favoriteIcon = document.createElement("img");
       favoriteIcon.src = "../assets/images/favoriteFull.png";
       favoriteButton.appendChild(favoriteIcon);
+
+      // Add click event to remove favorite
+      favoriteButton.addEventListener("click", () => {
+        removeFavoriteMovie(movie);
+      });
 
       const image = document.createElement("img");
       image.src = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
@@ -26,5 +31,17 @@ document.addEventListener("DOMContentLoaded", function () {
       card.appendChild(image);
       favoriteMoviesContainer.appendChild(card);
     });
+  }
+
+  // Function to remove a favorite movie
+  function removeFavoriteMovie(movieToRemove) {
+    let favoriteMovies =
+      JSON.parse(localStorage.getItem("favoriteMovies")) || [];
+    favoriteMovies = favoriteMovies.filter(
+      (movie) => movie.id !== movieToRemove.id
+    );
+    localStorage.setItem("favoriteMovies", JSON.stringify(favoriteMovies));
+    // Redisplay favorite movies
+    displayFavoriteMovies(favoriteMovies);
   }
 });
