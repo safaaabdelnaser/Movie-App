0b0817cbf612b16204e895f3c619b376c4b43875
safaa abdel naser <55063496+safaaabdelnaser@users.noreply.github.com>
2024-03-15 00:49:12 +0200

    Update Header.js

--- js/Header.js
+++ js/Header.js
@@ -1,5 +1,5 @@
 var header_content =
-  '<div class="logo"><h2>Movie<span> Site</span></h2></div><nav><ul><li><a href="../pages/Movies.html">Movies</a></li><li><a href="../pages/Favorite.html">Favorite</a></li><li><a href="../pages/Chat.html">Chat</a></li><li><a href="../index.html">Login/Register</a></li></ul></nav>';
+  '<div class="logo"><h2>Movie<span> Site</span></h2></div><nav><ul><li><a href="../pages/movies.html">Movies</a></li><li><a href="../pages/Favorite.html">Favorite</a></li><li><a href="../pages/Chat.html">Chat</a></li><li><a href="../index.html">Login/Register</a></li></ul></nav>';
 var header_element = document.getElementById("header");
 header_element.innerHTML = header_content;
 console.log(header_content);
